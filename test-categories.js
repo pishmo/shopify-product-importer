a2cf0bfa69e9f56329a8c00baabd829d8f8a00ab
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-16 02:33:16 +0200

    Update test-categories.js

--- test-categories.js
+++ test-categories.js
@@ -19,7 +19,7 @@ async function fetchMonofilamentProducts() {
   let hasMorePages = true;
   
   try {
-    while (hasMorePages) {  // Лимит от 5 страници за тест
+    while (hasMorePages) {  
       console.log(`Fetching page ${page}...`);
       
       const response = await fetch(`${FILSTAR_API_BASE}/products?page=${page}&limit=1000`, {
