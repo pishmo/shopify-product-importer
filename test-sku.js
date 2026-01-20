aaa96902deed13cc69b928eb8f2ba779298ff1c6
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-20 23:10:23 +0200

    Update test-sku.js

--- test-sku.js
+++ test-sku.js
@@ -9,7 +9,7 @@ const FILSTAR_API_BASE = 'https://filstar.com/api';
 
 // SKU-та за тестване
 const TEST_SKUS = [
-  '963102',
+  '963492',
   // Добави още SKU-та тук ако искаш да тестваш повече
 ];
 
