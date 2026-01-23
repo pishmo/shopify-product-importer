10bb6ee72f4c0dc44c7b6714e5aa7206be40aafd
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-23 18:19:58 +0200

    Update import-fishing-accessories.js

--- import-fishing-accessories.js
+++ import-fishing-accessories.js
@@ -53,7 +53,7 @@ const stats = {
 
 // TEST MODE - само за 1 категория
 const TEST_MODE = true;
-const TEST_CATEGORY = 'pike_and_catfish';
+const TEST_CATEGORY = 'pole_and_match';
 
 // Функция за нормализация на изображения
 async function normalizeImage(imageUrl, sku) {
