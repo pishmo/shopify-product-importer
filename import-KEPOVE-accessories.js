9ea86a08f869e9a6fcdfb6269b05aa1b42634fc6
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-26 04:00:46 +0200

    Update import-KEPOVE-accessories.js

--- import-KEPOVE-accessories.js
+++ import-KEPOVE-accessories.js
@@ -23,6 +23,8 @@ const FILSTAR_ACCESSORIES_CATEGORY_IDS = {
 
 // Shopify collection IDs - САМО 4 КАТЕГОРИИ
 const SHOPIFY_ACCESSORIES_COLLECTIONS = {
+
+  
   pike_and_catfish: 'gid://shopify/Collection/739661185406',
   pole_and_match: 'gid://shopify/Collection/739661218174',
   knives: 'gid://shopify/Collection/739661250942',
