b62e3e476c5165baf1e9f168ce3a273803b54c52
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-28 13:21:21 +0200

    Update import-fishing-LINES.js

--- import-fishing-LINES.js
+++ import-fishing-LINES.js
@@ -382,7 +382,7 @@ function getCategoryType(product) {
   for (const category of product.categories) {
     const categoryId = category.id?.toString();
     
-    for (const [type, ids] of Object.entries(FILSTAR_ACCESSORIES_CATEGORY_IDS)) {
+    for (const [type, ids] of Object.entries(FILSTAR_LINE_CATEGORY_IDS)) {
       if (ids.includes(categoryId)) {
         return type;
       }
