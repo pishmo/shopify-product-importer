b349f3e665fcf4edf3841a84528cf7f4a69d402f
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-29 21:44:38 +0200

    Update import-fishing-REELS.js

--- import-fishing-REELS.js
+++ import-fishing-REELS.js
@@ -1091,17 +1091,6 @@ async function main() {
     });
 
 
-
-
- // филтър ску
-const testSkus = ['930206'];
-accessoryProducts = accessoryProducts.filter(p => 
-  p.variants?.some(v => testSkus.includes(v.sku))
-);
-console.log(`?? Filtered to ${accessoryProducts.length} test products\n`);
-// край на филтъра
-
-
     
     
     console.log(`🎯 Found ${accessoryProducts.length} accessory products to process\n`);
