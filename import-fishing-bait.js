0bf5f09afd622d370ba564e70ca702289c0f3d7c
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-29 16:43:19 +0200

    Update import-fishing-bait.js

--- import-fishing-bait.js
+++ import-fishing-bait.js
@@ -1092,23 +1092,7 @@ console.log('  - Други захранки - Категория Id - (77)');
       return categoryType !== null;
     });
 
-
-
-
- // филтър ску
-const testSkus = ['963252', '962013','960594','952471','954949', '960415'];
-accessoryProducts = accessoryProducts.filter(p => 
-  p.variants?.some(v => testSkus.includes(v.sku))
-);
-console.log(`?? Filtered to ${accessoryProducts.length} test products\n`);
-// край на филтъра
-
-
-
-
-
-    
-    
+        
     console.log(`🎯 Found ${accessoryProducts.length} accessory products to process\n`);
 
 
