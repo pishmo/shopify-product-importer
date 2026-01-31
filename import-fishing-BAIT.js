8b453a09d76d4b5aceec75998b5cb1a6e27baab2
pishmo <30525232+pishmo@users.noreply.github.com>
2026-01-31 06:00:42 +0200

    Update import-fishing-BAIT.js

--- import-fishing-BAIT.js
+++ import-fishing-BAIT.js
@@ -360,7 +360,7 @@ function getCategoryType(product) {
   for (const category of product.categories) {
     const categoryId = category.id?.toString();
     
-    for (const [type, ids] of Object.entries(FILSTAR_RODS_CATEGORY_IDS )) {
+    for (const [type, ids] of Object.entries(FILSTAR_BAIT_CATEGORY_IDS)) {
       if (ids.includes(categoryId)) {
         return type;
       }
