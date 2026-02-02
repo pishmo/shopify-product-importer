cd227052425fbf3f0de0abb5f6d996782b1d45e8
pishmo <30525232+pishmo@users.noreply.github.com>
2026-02-02 05:01:05 +0200

    Update import-fishing-RODS.js

--- import-fishing-RODS.js
+++ import-fishing-RODS.js
@@ -1277,13 +1277,7 @@ if (categoryType && stats[categoryType]) {
       return categoryType !== null;
     });
 
-// филтър ску
-const testSkus = ['932605'];
-accessoryProducts = accessoryProducts.filter(p => 
-  p.variants?.some(v => testSkus.includes(v.sku))
-);
-console.log(`?? Filtered to ${accessoryProducts.length} test products\n`);
-// край на филтъра
+
 	  
     console.log(`🎯 Found ${accessoryProducts.length} products to process\n`);
     
