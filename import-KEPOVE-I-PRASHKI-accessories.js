e3ef586767ddd592487ab094a5345b11d2ffbbd1
pishmo <30525232+pishmo@users.noreply.github.com>
2026-02-03 08:16:35 +0200

    Update import-KEPOVE-I-PRASHKI-accessories.js

--- import-KEPOVE-I-PRASHKI-accessories.js
+++ import-KEPOVE-I-PRASHKI-accessories.js
@@ -371,10 +371,8 @@ function getCategoryType(product) {
 // Функция за получаване на име на категория
 function getCategoryName(categoryType) {
   const names = {
-    pike_and_catfish: 'Аксесоари щука и сом',
-    pole_and_match: 'Аксесоари щека и мач',
-    knives: 'Ножове',
-    chairs_umbrellas_tents: 'Столове и палатки'  
+       ceps: 'Живарници и кепове',
+       prashki: 'Прашки'
    
   };
   
