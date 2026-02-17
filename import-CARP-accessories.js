b721ad312453284c741af9a965c01a1cea0b76cb
pishmo <30525232+pishmo@users.noreply.github.com>
2026-02-17 19:37:38 +0200

    Update import-CARP-accessories.js

--- import-CARP-accessories.js
+++ import-CARP-accessories.js
@@ -1528,14 +1528,6 @@ async function updateShopifyProduct(shopifyProduct, filstarProduct, categoryType
 
 // място за филтъра
 
-
- // Филтър за конкретни SKU (ако е нужно)
-    const targetSkus = ['957846']; // Замени с реалните SKU-та
-    accessoryProducts = accessoryProducts.filter(product => 
-      product.variants && product.variants.some(v => targetSkus.includes(v.sku))
-    );
-
-
 	  
 // то тук филтър
 
