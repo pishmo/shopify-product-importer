f3b80b9891c71e96edf4e90a9a9328b2a2b695a1
pishmo <30525232+pishmo@users.noreply.github.com>
2026-02-17 22:50:13 +0200

    Update import-BOX-accessories.js

--- import-BOX-accessories.js
+++ import-BOX-accessories.js
@@ -1524,12 +1524,6 @@ async function updateShopifyProduct(shopifyProduct, filstarProduct, categoryType
 
 // място за филтъра
 
-	  
- // Филтър за конкретни SKU (ако е нужно)
-    const targetSkus = ['957893']; // Замени с реалните SKU-та
-    accessoryProducts = accessoryProducts.filter(product => 
-      product.variants && product.variants.some(v => targetSkus.includes(v.sku))
-    );
 
 
 // то тук филтър
