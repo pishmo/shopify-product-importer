cebece055dad0b12b7ff99ce8705054166f052dd
pishmo <30525232+pishmo@users.noreply.github.com>
2026-02-19 23:58:02 +0200

    Update import-fishing-REELS.js

--- import-fishing-REELS.js
+++ import-fishing-REELS.js
@@ -1529,7 +1529,7 @@ async function updateShopifyProduct(shopifyProduct, filstarProduct, categoryType
 
 
  // Филтър за конкретни SKU (ако е нужно)
-    const targetSkus = ['935694']; // Замени с реалните SKU-та
+    const targetSkus = ['947788']; // Замени с реалните SKU-та
     accessoryProducts = accessoryProducts.filter(product => 
       product.variants && product.variants.some(v => targetSkus.includes(v.sku))
     );
