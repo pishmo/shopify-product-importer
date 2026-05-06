d96308115cd643c4d1fff54a4118cae0e79de977
pishmo <30525232+pishmo@users.noreply.github.com>
2026-05-07 01:47:00 +0300

    Update import-KAMPING.js

--- import-KAMPING.js
+++ import-KAMPING.js
@@ -1478,15 +1478,6 @@ async function updateShopifyProduct(shopifyProduct, filstarProduct, categoryType
 // място за филтъра
 
 	
-
- // Филтър за конкретни SKU (ако е нужно)
-// Филтър за конкретни SKU (ако е нужно)
-    const targetSkus = ['963810']; // Замени с реалните SKU-та
-    accessoryProducts = accessoryProducts.filter(product => 
-      product.variants && product.variants.some(v => targetSkus.includes(v.sku))
-    );
-
-
 	  
 // то тук филтър
 
