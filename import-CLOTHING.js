3be208be490fed0f9762c1c3e2d304015212d0dc
pishmo <30525232+pishmo@users.noreply.github.com>
2026-05-07 02:09:54 +0300

    Update import-CLOTHING.js

--- import-CLOTHING.js
+++ import-CLOTHING.js
@@ -1516,6 +1516,12 @@ async function updateShopifyProduct(shopifyProduct, filstarProduct, categoryType
 
 // място за филтъра
 
+	
+		// Филтър за конкретни SKU (ако е нужно)
+    const targetSkus = ['960120']; // Замени с реалните SKU-та
+    accessoryProducts = accessoryProducts.filter(product => 
+      product.variants && product.variants.some(v => targetSkus.includes(v.sku))
+    );
 
   
 
